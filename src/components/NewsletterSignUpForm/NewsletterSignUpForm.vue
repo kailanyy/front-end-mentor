364e1218552f0ebd6778f89b8ebb2c815f615305
Kailany <kailanyiaramenco@gmail.com>
2023-07-12 14:01:00 -0300

    First version of newsletter

--- src/components/NewsletterSignUpForm/NewsletterSignUpForm.vue
+++ src/components/NewsletterSignUpForm/NewsletterSignUpForm.vue
@@ -1,74 +1,90 @@
 <template>
-  <div class="bg-[#36384d] h-full w-full flex justify-center items-center">
-    <div class="bg-slate-50 rounded-3xl p-4 flex items-stretch">
+  <div v-if="subscribed === false" class="bg-newsletter-charcoal-grey h-full w-full flex justify-center items-center">
+    <div class="bg-newsletter-white rounded-3xl p-4 flex items-stretch">
       <div class="max-w-sm p-8">
-        <h1 class="text-[#232742] text-5xl mb-4 font-bold font-roboto">Stay updated!</h1>
+        <h1 class="text-newsletter-charcoal-grey text-5xl mb-4 font-bold font-roboto">Stay updated!</h1>
         <p class="mb-4">Join 60,000+ product managers receiving monthly updates on:</p>
-
         <ul>
           <li v-for="item in items" :key="item.message" class="flex items-stretch text-sm mb-2">
-            <img class="mr-2" :src="imagePath"> {{ item.message }}
+            <img class="mr-2" src="@/assets/NewsletterSignUpForm/images/icon-list.svg" alt=""> {{ item.message }}
           </li>
         </ul>
 
-        <h1 class="font-semibold text-sm mb-2 mt-8">
-          Email address
-        </h1>
-
-        <div>
-          <input type="text" placeholder="email@company.com" v-model="email"
-            class="outline-0 border-2 w-full h-12 rounded-lg border-inherit p-3.5 mb-4">
-            <div class="input-errors" v-for="error of v$.email.$errors" :key="error.$uid">
-              <div class="error-msg">{{ error.$message }}</div>
-            </div>
+        <div class="mb-2 mt-8 text-sm flex justify-between">
+          <h1 class="font-semibold">
+            Email address
+          </h1>
+          <div class="input-errors" v-for="error of v$.email.$errors" :key="error.$uid">
+            <div class="error-msg text-newsletter-tomato">{{ error.$message }}</div>
+          </div>
         </div>
 
-        <button @click.prevent="subscribe" class="bg-[#232742] rounded-lg text-white p-3.5 font-semibold w-full text-sm">
+        <div :class="{ error: v$.email.$errors.length }">
+          <input type="text" placeholder="email@company.com" v-model="state.email"
+            class="outline-0 border w-full h-12 rounded-lg border-inherit p-3.5 mb-4"
+            :class="[v$.email.$errors.length > 0 ? 'bg-[#ff44442a] border-newsletter-dark-slate-grey placeholder-newsletter-dark-slate-grey' : 'border-newsletter-grey placeholder-newsletter-grey']">
+        </div>
+
+        <button @click.prevent="subscribe"
+          class="bg-newsletter-charcoal-grey rounded-lg text-newsletter-white p-3.5 font-semibold w-full text-sm hover:bg-newsletter-tomato focus:outline-none">
           Subscribe to monthly newsletter
         </button>
       </div>
-
       <div>
-        <img width="324" src="@/assets/NewsletterSignUpForm/images/illustration-sign-up-desktop.svg">
+        <img  src="@/assets/NewsletterSignUpForm/images/illustration-sign-up-desktop.svg">
       </div>
     </div>
   </div>
+  <template v-else>
+    <NewsletterSuccess :email="state.email" />
+  </template>
 </template>
 
 <script>
-import iconList from "@/assets/NewsletterSignUpForm/images/icon-list.svg"
+import { ref, reactive, computed } from 'vue';
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, helpers } from '@vuelidate/validators'
+import NewsletterSuccess from './NewsletterSuccess.vue';
 
 export default {
-  methods: {
-    subscribe() {
-      this.subscribed = true
-      console.log('email:', this.email);
-    }
+  components: {
+    NewsletterSuccess
   },
+
   setup() {
-    return { v$: useVuelidate() }
-  },
-  data() {
-    return {
-      subscribed: false,
-      imagePath: iconList,
-      firstname: "",
-      items: [
-        { message: "Product discovery and building what matters" },
-        { message: "Measuring to ensure updates are a success" },
-        { message: "And much more!" }
-      ],
-      email: '',
-      validations() {
-        return {
-          firstname: { required },
-          email: { required },
-        }
+
+    const subscribed = ref(false);
+
+    const state = reactive({
+      props: {
+        email: ''
       }
+    })
+
+    const rules = computed(() => {
+      return {
+        email: { required: helpers.withMessage('Valid email required', required) }
+      }
+    })
+
+    const v$ = useVuelidate(rules, state)
+
+    const items = ([
+      { message: "Product discovery and building what matters" },
+      { message: "Measuring to ensure updates are a success" },
+      { message: "And much more!" },
+    ])
+
+    async function subscribe() {
+      await v$.value.$validate();
+      subscribed.value = true
     }
+
+    return { subscribe, subscribed, state, v$, items, NewsletterSuccess, email }
   }
 }
 
 </script>
+
 
 <style></style>
---
+++ src/components/NewsletterSignUpForm/NewsletterSuccess.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-newsletter-charcoal-grey h-full w-full flex justify-center items-center">
+    <div class="bg-newsletter-white rounded-3xl p-4 flex items-stretch">
+      <div class="max-w-sm p-4">
+        <img src="@/assets/NewsletterSignUpForm/images/icon-list.svg" width="40" class="mb-4">
+        <h1 class="text-newsletter-charcoal-grey text-5xl mb-4 font-bold font-roboto">Thanks for subscribing!</h1>
+        <p class="mb-4">A confirmation email has been sent to
+          <strong>{{ email }}</strong>. Please open it and click the
+          button inside to confirm your subscription.
+        </p>
+        <button
+          class="bg-newsletter-charcoal-grey rounded-lg text-newsletter-white p-3.5 font-semibold w-full text-sm hover:bg-newsletter-tomato focus:outline-none">
+          Dismiss message
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    email: ''
+  }
+}
+</script>
+
+<style></style>
