4580c0f74cdfdd97c43658cd2811ee88452f63ab
Kailany <kailanyiaramenco@gmail.com>
2023-10-27 13:50:02 -0300

    dropdown component and using modal in crowdfunding page

--- src/components/crowdfunding-product-page-main/Dropdown.vue
+++ src/components/crowdfunding-product-page-main/Dropdown.vue
@@ -1,24 +1,31 @@
 <template>
-  <div @click="open = !open" class="cursor-pointer">
-    <img v-if="!open" class="md:hidden sm:block" src="./starter/images/icon-hamburger.svg">
-    <img v-else class="md:hidden sm:block" src="./starter/images/icon-close-menu.svg">
-  </div>
-  <div class="flex absolute bg-white rounded-md top-10 shadow-md">
-    <div v-if="open" class="flex flex-col justify-center items-start w-48 h-28 m-3 text-black">
-      <span class="cursor-pointer mb-4">About</span>
-      <span class="cursor-pointer mb-4">Discover</span>
-      <span class="cursor-pointer">Get Started</span>
+  <div class="z-50 absolute flex md:hidden sm:block">
+    <img @click="toggle" class="md:hidden sm:block cursor-pointer" src="./starter/images/icon-hamburger.svg">
+    <div v-show="dropdownActive"
+      class="bg-white text-black font-commissionerRegular z-50 flex flex-col justify-center absolute rounded-md shadow-md h-[150px] w-[200px] right-0 top-10">
+      <slot />
     </div>
   </div>
 </template>
-
+  <!-- <div @click="open = !open" class="cursor-pointer">
+    <img v-if="!open" class="md:hidden sm:block" src="./starter/images/icon-hamburger.svg">
+    <img v-else class="md:hidden sm:block" src="./starter/images/icon-close-menu.svg">
+  </div>
+  
+  <div class="flex absolute">
+    <div v-if="open"">
+    </div>
+  </div> -->
 <script>
 export default {
-  data() {
-    return { open: false }
+  props: ["dropdownActive"],
+  setup(props, { emit }) {
+    const toggle = () => {
+      emit('toggle')
+    }
+    return { toggle, open: false }
   }
 }
 </script>
 
-<style scoped>
-</style>
+<style></style>
--- src/components/crowdfunding-product-page-main/Modal.vue
+++ src/components/crowdfunding-product-page-main/Modal.vue
@@ -1,75 +1,29 @@
 <template>
-  <div>
-    <div @click="open = !open">
-      <button
-        class="flex justify-center items-center h-14 w-44 rounded-full bg-moderate-cyan hover:dark-cyan duration-200 text-white text-sm">
-        Back this project
-      </button>
-    </div>
-    <div v-if="open" class="rounded-md p-10 shadow-md">
-      <h1 class="font-commissionerBold text-lg  mb-2">Back this project</h1>
-      <p class="text-sm text-dark-gray mb-6 font-commissionerRegular">
-        Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?
-      </p>
-      <div v-for="card in cards" :key="card.title" class="border-2 rounded-md p-7 mb-4">
-        <div class="flex flex-row justify-between">
-          <div class="flex flex-row gap-4">
-            <input type="radio" class="h-6 w-6 mr-2">
-            <h1 class="font-commissionerBold">{{ card.title }}</h1>
-            <h1 class="text-moderate-cyan font-commissionerBold">{{ card.pledge }}</h1>
-          </div>
-          <h1 class="font-commissionerBold text-2xl sm:mb-4"> {{ card.amount }}
-            <span class="font-commissionerRegular text-dark-gray text-sm">left</span>
-          </h1>
-        </div>
-        <p class="text-sm text-dark-gray">
-          {{ card.description }}
-        </p>
-        <div class="flex flex-row items-center">
-          <h1 class="text-dark-gray">
-            Enter your plegde
-          </h1>
-          <div>
-            <button
-              class="flex p-2 font-commissionerBold text-dark-gray flex-row items-center border-moderate-cyan rounded-full bg-white border-[1.5px] h-12">
-              <p class="ml-4">
-                $
-              </p>
-              <input type="number" class="w-16 outline-none text-black text-center">
-            </button>
-            <button class="h-12 w-28 rounded-full bg-moderate-cyan hover:dark-cyan duration-200 text-white text-sm">
-              Continue
-            </button>
-          </div>
-
-        </div>
-      </div>
-      <div class="top-0 left-0 -z-10 absolute bg-black opacity-30"></div>
-    </div>
+  <div v-show="modalActive"
+    class="overflow-scroll flex flex-col items-center fixed z-50 bg-white rounded-md p-10 m-10 shadow-md">
+    <img @click="close" class="cursor-pointer absolute left-[95%]" src=".../../starter/images/icon-close-modal.svg"
+      alt="icon close modal">
+    <slot />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
+import { watch } from 'vue'
 export default {
-  props: {
-    cards: {
-      type: Array,
-      default: () => {
-        return []
+  props: ["modalActive"],
+  setup(props, { emit }) {
+
+
+    watch(
+      () => props.modalActive,
+      () => {
+        console.log("Valor modal mudou, ", props.modalActive)
       }
-    }
-  },
-
-  setup() {
-    const open = ref(false);
-
+    )
     const close = () => {
-      open.value = false;
+      emit('close')
     }
-
-    return { open, close }
+    return { close }
   }
 }
 </script>
